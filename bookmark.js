c3934222490eb4b771e0455f26b4b3418e7b4e65
YukiHime <36097203+Numseven@users.noreply.github.com>
2019-12-03 15:38:11 +0800

    Create bookmark.js

---
+++ bookmark.js
@@ -0,0 +1,58 @@
+var search_list = "<table><thead><tr><th>Book Name</th><th>Link</th><th>Delete</th></tr></thead>";
+var keys = [];
+var name;
+var tag = 0;
+function start() {
+    document.getElementById("bookmarks").innerHTML = search_list + "</table>";
+    loadsearch();
+}
+function add() {
+    if (document.getElementById("name").value === "") {
+        alert("請輸入標籤名稱");
+        document.getElementById("name").value = "";
+        return;
+    }
+    var name = "hentai-" + document.getElementById("name").value;
+    var Url = document.getElementById("URL").value;
+    localStorage.setItem(name, Url);
+    document.getElementById("name").value = "";
+    document.getElementById("URL").value = "";
+    loadsearch();
+}
+function loadsearch() {
+    var length = localStorage.length;
+    search_list = "<table><thead><tr><th>Book Name</th><th>Link</th><th>Delete</th></tr></thead>";
+    for (var i = 0; i < length; ++i) {
+        keys[i] = localStorage.key(i);
+    }
+    keys.sort();
+    for (tag in keys) {
+        if (keys[tag].startsWith("hentai-")) {
+            name = keys[tag];
+            search_list += "<tr><td>" + keys[tag].substring(7) + "</td><td><a href='https://nhentai.net/g/" + localStorage.getItem(keys[tag]) + "/1/' target='_blank'>" + localStorage.getItem(keys[tag]) + "</a></td><td><input type='button' value='Delete' onclick='deleting()'></td></tr>";
+        }
+    }
+    document.getElementById("bookmarks").innerHTML = search_list + "</table>";
+}
+function deleting() {
+    localStorage.removeItem(keys[tag]);
+    loadsearch();
+}
+function remove() {
+    var confirm = window.prompt("Are you really sure about it?(Y for Yes, N for No)");
+    if (confirm === "Y") {
+        localStorage.clear();
+        search_list = "<table><thead><tr><th>Book Name</th><th>Link</th><th>Delete</th></tr></thead>";
+        document.getElementById("bookmarks").innerHTML = search_list + "</table>";
+        document.getElementById("name").value = "";
+        document.getElementById("URL").value = "";
+        tag = 0;
+    } else if (confirm === "N") {
+        return;
+    } else {
+        return;
+    }
+
+}
+
+window.addEventListener("load", start, false);
