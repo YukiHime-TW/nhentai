76711c8fbf54f2f620b31cf103128f2932dfc09b
YukiHime <36097203+Numseven@users.noreply.github.com>
2019-12-15 22:17:31 +0800

    Update bookmark.js

--- bookmark.js
+++ bookmark.js
@@ -1,7 +1,11 @@
 var search_list = "<table><thead><tr><th>Book Name</th><th>Link</th><th>Delete</th></tr></thead>";
-var keys = [];
-var name;
-var tag = 0;
+
+var bulletin = `<div id="bulletin_board"><span id="topic">&spades;公告&spades;</span><br>網站已全面更新<br>Email驗證後才可進入secret，否則只可使用一般模式<br></div>`;
+
+var withemailverify = ``;
+
+var noneemailverify = ``;
+
 var firebaseConfig = {
     apiKey: "AIzaSyAZM5RCY5TAiBYwEPselITtTA5Xgy-GPoY",
     authDomain: "nhentai-bookmark.firebaseapp.com",
@@ -23,7 +27,10 @@ function add() {
     }
 
     var name = document.getElementById("name").value;
+
     var Url = document.getElementById("URL").value;
+
+    var web = document.getElementById("option").value;
 
     var path = `/${user.uid}/nhentai/book/`;
 
@@ -64,20 +71,39 @@ function newuser() {
 
     var password = document.getElementById("PW").value;
 
-    firebase.auth().createUserWithEmailAndPassword(account, password).catch(function (error) {
+    firebase.auth().createUserWithEmailAndPassword(account, password).then(function () {
+
+        firebase.auth().currentUser.sendEmailVerification().then(function () {
+
+            alert("An verify email has been send");
+
+        }).catch(function (error) {
+
+            alert("An error has happened");
+
+        });
+
+    }).catch(function (error) {
 
         var errorCode = error.code;
+
         var errorMessage = error.message;
 
         if (errorCode == "auth/weak-password") {
+
             alert("The password is too weak");
+
         } else {
+
             alert(errorMessage);
+
         }
 
         console.log(error);
 
     });
+
+
 
 }
 
@@ -131,7 +157,8 @@ function initial() {
 
     firebase.auth().onAuthStateChanged(function (user) {
 
-        if (user) {
+
+        if (user && user.emailVerified) {
 
             var email = user.email;
 
@@ -139,13 +166,25 @@ function initial() {
 
             alert("You have been logged in as " + email + "!");
 
-            document.getElementById("main").innerHTML = 'Book name OR Your own tag: <input type="text" id="name" value="" required /><br><br>Numbers: <input type="text" id="URL" value="" required /><br><br><input type="button" id="add_new" value="Add New Book" onclick="add()">&nbsp;<a href="https://nhentai.net/language/chinese/" target="_blank"><input type="button" id="CHINESE" value="中文本本這邊請"></a><br><br><span id="highlighting" style="font-size: larger;">在刪除單筆資料後，請重新整理頁面以取得正確書籤</span><br><br><div id="bulletin_board"><span id="topic">&spades;公告&spades;</span><br><span id="highlighting">重要!!網站正在更新中，請勿新增任何新的本本!</span><br>請新用戶注意，一旦開始使用，您的電腦或是手機便<span id="highlighting">不可關機</span>，否則會導致資料消失<br>且請所有用戶注意，各瀏覽器之間的紀錄<span id="highlighting">不共通</span>，使用上需注意。<br>關於這幾點，開發者已經在著手學習資料庫的使用。<br>如果有任何操作上的更新，將會在這裡進行公告。<br></div><hr><div id="bookmarks"></div><br><input type="button" value="Log out" onclick="logout()">';
+            document.getElementById("main").innerHTML = `Book name OR Your own tag: <input type="text" id="name" value="" required /><br><br>Numbers: <input type="text" id="URL" value="" required /><br><br><input type="button" id="add_new" value="Add New Book" onclick="add()">&nbsp;<a href="https://nhentai.net/language/chinese/" target="_blank"><input type="button" id="CHINESE" value="中文本本這邊請"></a><br><br>${bulletin}<hr><div id="bookmarks"></div><br><input type="button" value="Log out" onclick="logout()">`;
+
+            loadsearch();
+
+        } else if (user) {
+
+            var email = user.email;
+
+            document.getElementById("sign").disabled = true;
+
+            alert("You have been logged in as " + email + "!");
+
+            document.getElementById("main").innerHTML = `Book name OR Your own tag: <input type="text" id="name" value="" required /><br><br>Numbers: <input type="text" id="URL" value="" required /><br><br><input type="button" id="add_new" value="Add New Book" onclick="add()">&nbsp;<a href="https://nhentai.net/language/chinese/" target="_blank"><input type="button" id="CHINESE" value="中文本本這邊請"></a><br><br>${bulletin}<hr><div id="bookmarks"></div><br><input type="button" value="Log out" onclick="logout()">`;
 
             loadsearch();
 
         } else {
 
-            document.getElementById("main").innerHTML = '<h2>漫畫筆記本</h2><div>帳號: <input type="text" id="EM" placeholder="Your Email" required　style="display:inline;width:auto;"><br><br>密碼: <input type="password" id="PW" placeholder="Password"　style="display:inline;width:auto;"><br><br><input type="submit" value="註冊" onclick="newuser()" id="newer">&nbsp;<input type="submit" value="登入" onclick="signin()" id="sign"></div>'
+            document.getElementById("main").innerHTML = `<h2>漫畫筆記本</h2><div>帳號: <input type="text" id="EM" placeholder="Your Email" required　style="display:inline;width:auto;"><br><br>密碼: <input type="password" id="PW" placeholder="Password" 　style="display:inline;"><br><br><input type="submit" value="註冊" onclick="newuser()" id="newer"><input type="submit" value="登入" onclick="signin()" id="sign"><br><br>需<span id="highlighting">註冊</span>後才可使用，只需提供電子郵件與密碼即可註冊<br>請注意，需進行<span id="highlighting">Email驗證</span>後才可使用內部機密<br></div>`
 
         }
 
