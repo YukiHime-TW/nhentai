fa6b1acc1dd51b6dbf51e7f617d713d80aba1fc2
YukiHime <36097203+Numseven@users.noreply.github.com>
2019-12-15 15:14:55 +0800

    Update bookmark.js

--- bookmark.js
+++ bookmark.js
@@ -87,6 +87,75 @@ function remove() {
 
 }
 
+function newuser() {
+    var account = document.getElementById("EM").value;
+    var password = document.getElementById("PW").value;
 
+    firebase.auth().createUserWithEmailAndPassword(account, password).catch(function (error) {
+
+        var errorCode = error.code;
+        var errorMessage = error.message;
+
+        if (errorCode == "auth/weak-password") {
+            alert("The password is too weak");
+        } else {
+            alert(errorMessage);
+        }
+
+        console.log(error);
+
+    });
+
+}
+
+function signin() {
+
+    if (firebase.auth().currentUser) {
+        firebase.auth().signOut();
+    } else {
+
+        var account = document.getElementById("EM").value;
+        var password = document.getElementById("PW").value;
+
+        firebase.auth().signInWithEmailAndPassword(account, password).catch(function (error) {
+
+            var errorCode = error.code;
+            var errorMessage = error.message;
+
+            if (errorCode === "auth/wrong-password") {
+                alert("Wrong password");
+            } else {
+                alert(errorMessage);
+            }
+
+            console.log(error);
+
+            document.getElementById("sign").disabled = false;
+
+        });
+
+    }
+
+    document.getElementById("sign").disabled = true;
+
+}
+
+function initial() {
+
+    firebase.auth().onAuthStateChanged(function (user) {
+
+        if (user) {
+
+            var email = user.email;
+
+            document.getElementById("sign").disabled = true;
+
+            alert("You have been logged in as " + email + "!");
+            
+        }
+
+    });
+
+}
 
 window.addEventListener("load", start, false);
